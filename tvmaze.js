4b586a751d20b31a8bfcb536be68f4354b4047fe
davisrao <davisrao1@gmail.com>
2021-09-01 18:02:36 -0700

    adding episodes functions

--- tvmaze.js
+++ tvmaze.js
@@ -4,51 +4,51 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
-const SEARCH_WEBSITE = "http://api.tvmaze.com/search/shows";
+const SHOW_SEARCH_WEBSITE = "http://api.tvmaze.com/search/shows";
 const DEFAULT_IMAGE = "https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300"
 
-/* takes in object of show data and returns the show ID*/
- function _getShowId(showData){
-  return showData.show.id;
-}
-/* takes in object of show data and returns the show name*/
- function _getShowName(showData){
-  return showData.show.name;
-}
-/* takes in object of show data and returns the show summary*/
- function _getShowSummary(showData){
-  return showData.show.summary;
-}
-/* takes in object of show data and returns the show image
-*    (if no image URL given by API, put in a default image URL)*/
- function _getShowImage(showData){
-  let showImage;
+// /* takes in object of show data and returns the show ID*/
+// function _getShowId(showData) {
+//   return showData.show.id;
+// }
+// /* takes in object of show data and returns the show name*/
+// function _getShowName(showData) {
+//   return showData.show.name;
+// }
+// /* takes in object of show data and returns the show summary*/
+// function _getShowSummary(showData) {
+//   return showData.show.summary;
+// }
+// /* takes in object of show data and returns the show image
+// *    (if no image URL given by API, put in a default image URL)*/
+// function _getShowImage(showData) {
 
-  if (showData.show.image === null){
-        showImage = DEFAULT_IMAGE
-      }
-      else{
-        showImage = showData.show.image.medium
-      }
-  return showImage;
-}
+
+
 
 /* takes in data from one show and returns an object of specific data*/
- function createObjectFromShow(show){
-  let showSummary = _getShowSummary(show);
-  let showImage = _getShowImage(show);
-  let showName = _getShowName(show);
-  let showId = _getShowId(show);
+function createObjectFromShow(showData) {
+  let showSummary = showData.show.summary
+  let showImage;
+  let showName = showData.show.name
+  let showId = showData.show.id;
 
-  return {id: showId,name: showName, summary: showSummary,image: showImage};
+  if (showData.show.image === null) {
+    showImage = DEFAULT_IMAGE
+  }
+  else {
+    showImage = showData.show.image.medium
+  }
+  return { id: showId, name: showName, summary: showSummary, image: showImage };
+
 }
 
 /* takes in data from all search responses and returns and array of objects with individual show data*/
- function createArrayOfObjects(shows){   //TV Maze search data
-  let showsData = shows.data;
+function createArrayOfObjects(shows) {   //TV Maze search data
+  // let showsData = shows.data;
   let arrayOfShowData = [];
 
-  for(let show of showsData){
+  for (let show of shows) {
     let showObject = createObjectFromShow(show)
     arrayOfShowData.push(showObject);
   }
@@ -64,9 +64,9 @@ const DEFAULT_IMAGE = "https://store-images.s-microsoft.com/image/apps.65316.135
 async function getShowsByTerm(searchTerm) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
   console.log("get shows by term: ", "this ran");
-  let response = await axios.get(SEARCH_WEBSITE, {params:{q:searchTerm}});
-  return response.data;
-
+  let response = await axios.get(SHOW_SEARCH_WEBSITE, { params: { q: searchTerm } });
+  let arrayOfShows = createArrayOfObjects(response.data);
+  return arrayOfShows;
 }
 
 
@@ -77,7 +77,7 @@ function populateShows(shows) {
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img 
               src=${show.image} 
@@ -94,10 +94,35 @@ function populateShows(shows) {
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
+async function getShowEpisodes(showId) {
+  //http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
+
+  let episodesAPI = `http://api.tvmaze.com/shows/${showId}/episodes`
+  let episodesResponse = await axios.get(episodesAPI);
+  let episodesResponseArray = episodesResponse.data;
+    // console.log("episodesresponse is:",episodesResponse,"episodesResponse type is: ",typeof episodesResponse)
+  let episodesArray = [];
+  for (let episodes of episodesResponseArray) {
+    episodesArray.push({ id: episodes.id, name: episodes.name, season: episodes.season, number: episodes.number });
+  }
+  console.log("episodes Array", episodesArray);
+  return episodesArray;
+}
+
+
+async function populateEpisodes(showId) {
+  console.log("the show id is:",showId)
+  let episodesArray = await getShowEpisodes(showId);
+
+  for (let episode of episodesArray) {
+    $('#episodesList').append(`<li>Season: ${episode.season} Episode: ${episode.number} Title: ${episode.name}<li/>`);
+  }
+}
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
@@ -106,18 +131,25 @@ async function searchForShowAndDisplay() {
   const term = $("#searchForm-term").val();
   const shows = await getShowsByTerm(term);
   console.log("shows", shows)
-  let showArray = createArrayOfObjects(shows);
-  console.log(showArray);
-
-
 
   $episodesArea.hide();
-  populateShows(showArray);
+  populateShows(shows);
 }
 
 $searchForm.on("submit", async function (evt) {
   evt.preventDefault();
   await searchForShowAndDisplay();
+});
+
+console.log("got to before click added to episode button");
+$('#showsList').on("click", ".Show-getEpisodes", async function (evt) {
+  // console.log("button works");
+  // console.log("this is the button click target",evt.target);
+  let showDiv = $(evt.target).closest('.Show');
+  console.log("showDiv is: ", showDiv);
+  let showId = showDiv.attr("data-show-id");
+  console.log("showId is:",showId);
+  await populateEpisodes(showId);
 });
 
 
